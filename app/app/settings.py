a2eebad6232db2bd15bd9f8c024b2e10e93e0593
ilkhom19 <ilkhomzhon.sidikov@gmail.com>
2023-01-17 17:18:06 +0600

    fixed "run shell not found" issue

--- app/app/settings.py
+++ app/app/settings.py
@@ -19,12 +19,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dq)60i5+@!z3adqcb2j2t-^)-_5+vse1bl1i+_3scdcu0)#8kh'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(',')
+    )
+)
 
 # Application definition
 
