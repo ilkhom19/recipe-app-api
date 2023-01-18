0abb9dd52a4783e5a259f352c63d0f7767595c9a
ilkhom19 <ilkhomzhon.sidikov@gmail.com>
2023-01-18 13:30:49 +0600

    added async email sending

--- app/app/settings.py
+++ app/app/settings.py
@@ -24,13 +24,13 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
-ALLOWED_HOSTS = ['52.198.226.89']
-# ALLOWED_HOSTS.extend(
-#     filter(
-#         None,
-#         os.environ.get('ALLOWED_HOSTS', '').split(',')
-#     )
-# )
+ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(',')
+    )
+)
 
 # Application definition
 
